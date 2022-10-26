cffea43929d8d64424e66e9a267b552def170b7c
Chris Tate <chris@austincodeshop.com>
2022-10-25 22:15:09 -0500

    init

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,11 +1,11 @@
 import { MongoClient } from 'mongodb';
 
 export const connect = async () => {
-    console.log(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSER}:27017/?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`);
+    console.log(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}:27017/?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`);
 
     console.log('Connecting via Mongo Client...');
     const client = await MongoClient.connect(
-        `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSER}:27017/?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`,
+        `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}:27017/?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`,
         {
             tlsCAFile: process.env.TLS_CA_FILE,
         },
