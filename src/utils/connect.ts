4f94e8f2696482f0e08030bc3c9b829358c8044c
Chris Tate <chris@austincodeshop.com>
2022-10-25 22:50:49 -0500

    init

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -7,11 +7,16 @@ export const connect = async () => {
         return cachedDb;
     }
 
-    console.log(
-        `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}:27017`,
-    );
+    console.log(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}:27017`);
 
     console.log('Connecting via Mongo Client...');
+    console.log({
+        readPreference: 'secondaryPreferred',
+        replicaSet: 'rs0',
+        retryWrites: false,
+        ssl: true,
+        sslCA: process.env.TLS_CA_FILE,
+    });
     const client = await MongoClient.connect(
         `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}:27017`,
         {
