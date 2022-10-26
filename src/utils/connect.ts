712b74ffcc7c2585e526a16cfa4074146e24828a
Chris Tate <chris@austincodeshop.com>
2022-10-25 22:33:39 -0500

    init

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -15,6 +15,7 @@ export const connect = async () => {
     const client = await MongoClient.connect(
         `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}:27017`,
         {
+            retryWrites: false,
             ssl: true,
             sslCA: process.env.TLS_CA_FILE,
         },
